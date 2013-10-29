54a5ab3fac046c14b30e6b0db9bab6f5d7b721fe
4Evergreen4 <eljohnson@frontier.com>
2013-10-29 14:42:20 -0400

    Fixed bedrock

--- init.lua
+++ init.lua
@@ -1,27 +1,37 @@
 minetest.register_node("minetest_tweaks:bedrock", {
-    tiles = {"minetest-tweaks_bedrock"},
+    tiles = {"minetest_tweaks_bedrock.png"},
     groups = {not_in_creative_inventory=1},
-    sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_ore({
     ore_type       = "scatter",
     ore            = "minetest_tweaks:bedrock",
     wherein        = "default:stone",
-    clust_scarcity = 2*2*2,
-    clust_num_ores = 8,
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
     clust_size     = 4,
-    height_min     = -2000,
-    height_max     = -2000,
+    height_min     = -1010,
+    height_max     = -1000,
 })
 
 minetest.register_ore({
     ore_type       = "scatter",
     ore            = "minetest_tweaks:bedrock",
     wherein        = "default:air",
-    clust_scarcity = 2*2*2,
-    clust_num_ores = 8,
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
     clust_size     = 4,
-    height_min     = -2000,
-    height_max     = -2000,
+    height_min     = -1010,
+    height_max     = -1000,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "minetest_tweaks:bedrock",
+    wherein        = "default:gravel",
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
+    clust_size     = 4,
+    height_min     = -1010,
+    height_max     = -1000,
 })
