7d1b42dd199d65c29741c9f7419c57a4d78d4050
4Evergreen4 <eljohnson@frontier.com>
2013-10-30 11:56:58 -0400

    Organized code a bit

---
+++ bedrock.lua
@@ -0,0 +1,39 @@
+--Bedrock Node
+minetest.register_node("minetest_tweaks:bedrock", {
+    tiles = {"minetest_tweaks_bedrock.png"},
+    groups = {not_in_creative_inventory=1},
+})
+
+--Generation of Bedrock
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "minetest_tweaks:bedrock",
+    wherein        = "default:stone",
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
+    clust_size     = 4,
+    height_min     = -1010,
+    height_max     = -1000,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "minetest_tweaks:bedrock",
+    wherein        = "default:air",
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
+    clust_size     = 4,
+    height_min     = -1010,
+    height_max     = -1000,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "minetest_tweaks:bedrock",
+    wherein        = "default:gravel",
+    clust_scarcity = 1.5*1.5*1.5,
+    clust_num_ores = 4,
+    clust_size     = 4,
+    height_min     = -1010,
+    height_max     = -1000,
+})
