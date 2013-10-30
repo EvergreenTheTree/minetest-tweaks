7b9c0a326bba80bc1c575b396e63365674437be5
4Evergreen4 <4Evergreen4@users.noreply.github.com>
2013-10-30 11:56:58 -0400

    Organized code a bit

--- init.lua
+++ init.lua
@@ -1,39 +1,15 @@
+-----------------------------Info-----------------------------------
+local title = "Minetest Tweaks"
+local version = "0.1 alpha"
+local mname = "minetest_tweaks"
+--------------------------------------------------------------------
+
+
+-----------------------------Dofiles--------------------------------
 dofile(minetest.get_modpath("minetest_tweaks").."/better_tools.lua")
+dofile(minetest.get_modpath("minetest_tweaks").."/bedrock.lua")
+--------------------------------------------------------------------
 
-minetest.register_node("minetest_tweaks:bedrock", {
-    tiles = {"minetest_tweaks_bedrock.png"},
-    groups = {not_in_creative_inventory=1},
-})
-
-minetest.register_ore({
-    ore_type       = "scatter",
-    ore            = "minetest_tweaks:bedrock",
-    wherein        = "default:stone",
-    clust_scarcity = 1.5*1.5*1.5,
-    clust_num_ores = 4,
-    clust_size     = 4,
-    height_min     = -1010,
-    height_max     = -1000,
-})
-
-minetest.register_ore({
-    ore_type       = "scatter",
-    ore            = "minetest_tweaks:bedrock",
-    wherein        = "default:air",
-    clust_scarcity = 1.5*1.5*1.5,
-    clust_num_ores = 4,
-    clust_size     = 4,
-    height_min     = -1010,
-    height_max     = -1000,
-})
-
-minetest.register_ore({
-    ore_type       = "scatter",
-    ore            = "minetest_tweaks:bedrock",
-    wherein        = "default:gravel",
-    clust_scarcity = 1.5*1.5*1.5,
-    clust_num_ores = 4,
-    clust_size     = 4,
-    height_min     = -1010,
-    height_max     = -1000,
-})
+------------------------Loaded Message------------------------------
+print("[Mod] "..title.." ["..version.."] ["..mname.."] Loaded...")
+--------------------------------------------------------------------
